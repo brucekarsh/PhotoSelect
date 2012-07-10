12d2e686d681659e40d6747e1853aeaaab4b5257
Bruce Karsh <brucekarsh@gmail.com>
2012-07-09 19:49:17 -0700

    Add some more code to OpenProjectWindow. It now has a scrollable list of radiobuttons. Eventually, these will represent project names.

--- src/OpenProjectWindow.h
+++ src/OpenProjectWindow.h
@@ -72,6 +72,47 @@ class OpenProjectWindow {
     gtk_widget_show(windowBox);
     gtk_container_add(GTK_CONTAINER(window), windowBox);
 
+    // Make a label (title_label) and put it in windowBox
+    GtkWidget *title_label = gtk_label_new("Open Project");
+    gtk_widget_show(GTK_WIDGET(title_label));
+    gtk_box_pack_start(GTK_BOX(windowBox), title_label, FALSE, FALSE, 0);
+
+    // Make a ScrolledWindow (scrolled_window) and put it in the windowBox
+    GtkWidget *scrolled_window = gtk_scrolled_window_new(NULL, NULL);
+    gtk_widget_show(GTK_WIDGET(scrolled_window));
+    gtk_box_pack_start(GTK_BOX(windowBox), scrolled_window, TRUE, TRUE, 0);
+
+    // Make a vbox (scrolled_vbox) and put it in the scrolled_window
+    GtkWidget *scrolled_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_widget_show(GTK_WIDGET(scrolled_vbox));
+    gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW(scrolled_window), scrolled_vbox);
+
+    // Make some buttons (quit_button, accept_button) and put them in an hbox (button_hbox) and
+    // put the hbox in windowBox
+
+    GtkWidget *quit_button = gtk_button_new_with_label("Quit");
+    GtkWidget *accept_button = gtk_button_new_with_label("Accept");
+    GtkWidget *button_hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+    gtk_widget_show(GTK_WIDGET(quit_button));
+    gtk_widget_show(GTK_WIDGET(accept_button));
+    gtk_widget_show(GTK_WIDGET(button_hbox));
+    gtk_box_pack_start(GTK_BOX(windowBox), button_hbox, FALSE, FALSE, 0);
+    gtk_box_pack_end(GTK_BOX(button_hbox), accept_button, FALSE, FALSE, 0);
+    gtk_box_pack_end(GTK_BOX(button_hbox), quit_button, FALSE, FALSE, 0);
+
+    GtkWidget* group = NULL;
+    GtkWidget* l;
+    for (int i=0;i<10;i++) {
+      if (NULL == group) {
+        l = gtk_radio_button_new_with_label(NULL, "Label");
+        group = l;
+      } else {
+        l = gtk_radio_button_new_with_label_from_widget(GTK_RADIO_BUTTON(group), "Label");
+      }
+      gtk_widget_show(GTK_WIDGET(l));
+      gtk_box_pack_start(GTK_BOX(scrolled_vbox), l, FALSE, FALSE, 0);
+    }
+
     g_signal_connect(window, "destroy", G_CALLBACK(quit_button_clicked_cb), NULL);
     g_signal_connect(quitButton, "clicked", G_CALLBACK(quit_button_clicked_cb), NULL);
 
